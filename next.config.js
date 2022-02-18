0337c5b19fa015c01ab709312a13885e56e1c229
Franco Alemandi <francolp59@gmail.com>
2022-02-18 14:19:42 -0300

    fix 404 on refresh

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  trailingSlash: true,
   // Esto para poder leer el .env. Hay alguna forma más segura de hacerlo?
   env: {
     URL_AIRTABLE_TOKEN: process.env.URL_AIRTABLE_TOKEN,
