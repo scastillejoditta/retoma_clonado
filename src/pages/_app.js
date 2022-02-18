29cf44798eff83f260f03572fd71b9248646a857
Franco Alemandi <francolp59@gmail.com>
2022-02-18 14:32:51 -0300

    update next, new config

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,12 @@
 import Layout from '../components/Layout'
-import Head from 'next/head';
+import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
   <Layout>
-    <Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139327370-1"></script>
-      <script
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=UA-139327370-1"></Script>
+      <Script
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
@@ -18,7 +17,7 @@ function MyApp({ Component, pageProps }) {
           `,
         }}
       />
-      <script dangerouslySetInnerHTML={{__html: `(function(h,o,t,j,a,r){
+      <Script dangerouslySetInnerHTML={{__html: `(function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:2687476,hjsv:6};
         a=o.getElementsByTagName('head')[0];
@@ -26,7 +25,6 @@ function MyApp({ Component, pageProps }) {
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}} />
-    </Head>
     <Component {...pageProps} />
   </Layout>
   )
