e772f246f085d2815d784856dd2fcd5dc0765fa8
Franco Alemandi <francolp59@gmail.com>
2022-02-23 18:38:57 -0300

    fix ga

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,12 @@
-// import Layout from '../components/Layout'
 import Head from 'next/head'
-import Script from 'next/script'
 import dynamic from 'next/dynamic'
 import '../styles/globals.css'
+
+import "../styles/globals.css";
+import {useEffect} from "react";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import * as gtag from '../lib/gtag'
 
 const Layout = dynamic(
   () => import('../components/Layout'),
@@ -10,40 +14,68 @@ const Layout = dynamic(
 )
 
 function MyApp({ Component, pageProps }) {
-  return (
-  <Layout>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
-        <meta name="description" content="Retoma es una red de conversación que busca entender las agendas de los jóvenes que participaron del Paro Nacional 2021, posicionarlas en la opinión pública e incidir en las agendas políticas de los candidatos para las elecciones del 2022."></meta>
-        <meta name="author" content="MUTANTE - https://www.mutante.org/sobre-mutante/quienes-somos" />
-        <meta name="keywords" content="Movilización, Transformación social, Incidencia política, Elecciones Colombia, Prosperidad social" />
-        <meta name="copyright" content="MUTANTE" />
-        <title>Retoma :: Red Informativa para la Toma de decisiones Democráticas</title>
-        <link rel="shortcut icon" href="/favicon.ico" />
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
-        <Script
-           id="google-analytics"
-           src="https://www.googletagmanager.com/gtag/js?id=G-L2CMG1D4Q2"
-           onLoad={() => {
-             window.dataLayer = window.dataLayer || [];
-             function gtag(){dataLayer.push(arguments);}
-             gtag('js', new Date());
-             gtag('config', 'G-L2CMG1D4Q2');
-           }}
-         />
-      </Head>
-      <Script
-        id="google-analytics"
-        src="https://www.googletagmanager.com/gtag/js?id=G-L2CMG1D4Q2"
-        onLoad={() => {
+  return (
+  <>
+    {/* Global Site Tag (gtag.js) - Google Analytics */}
+    <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+    />
+    <Script
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-L2CMG1D4Q2');
+          gtag('config', '${gtag.GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `,
+      }}
+    />
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
+      <meta name="description" content="Retoma es una red de conversación que busca entender las agendas de los jóvenes que participaron del Paro Nacional 2021, posicionarlas en la opinión pública e incidir en las agendas políticas de los candidatos para las elecciones del 2022."></meta>
+      <meta name="author" content="MUTANTE - https://www.mutante.org/sobre-mutante/quienes-somos" />
+      <meta name="keywords" content="Movilización, Transformación social, Incidencia política, Elecciones Colombia, Prosperidad social" />
+      <meta name="copyright" content="MUTANTE" />
+      <title>Retoma :: Red Informativa para la Toma de decisiones Democráticas</title>
+      <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
         }}
       />
-    <Component {...pageProps} />
-  </Layout>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </>
   )
 }
 
